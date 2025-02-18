5f008d83401ae235f39514e5ba109292591f5646
arthurnov <art.dsky@gmail.com>
2025-02-19 00:19:47 +0200

    ch6

--- src/cell.py
+++ src/cell.py
@@ -17,6 +17,8 @@ class Cell:
         self._win = window
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
