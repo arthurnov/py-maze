8db221b88b28ac41a333830e00ffbf9d2bf414cd
arthurnov <art.dsky@gmail.com>
2025-02-21 19:35:45 +0200

    ch7 pt2

--- src/maze.py
+++ src/maze.py
@@ -12,29 +12,33 @@ class Maze:
         cell_size_y,
         win,
     ):
-        self.x1 = x1
-        self.y1 = y1
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.cell_size_x = cell_size_x
-        self.cell_size_y = cell_size_y
-        self.win = win
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
 
         self._create_cells()
 
     def _create_cells(self):
-        self._cells = [[Cell(self.win)] * self.num_rows] * self.num_cols
+        self._cells = [[Cell(self._win)] * self._num_rows] * self._num_cols
         for i in range(len(self._cells)):
             for j in range(len(self._cells[i])):
                 self._draw_cell(i, j)
         
 
     def _draw_cell(self, i, j):
-        cell_x = i * self.cell_size_x + self.x1
-        cell_y = j * self.cell_size_y + self.y1
-        self._cells[i][j].draw(cell_x, cell_y, cell_x + self.cell_size_x, cell_y + self.cell_size_y)
+        if self._win is None:
+            return
+        cell_x = i * self._cell_size_x + self._x1
+        cell_y = j * self._cell_size_y + self._y1
+        self._cells[i][j].draw(cell_x, cell_y, cell_x + self._cell_size_x, cell_y + self._cell_size_y)
         self._animate()
 
     def _animate(self):
-        self.win.redraw()
-        time.sleep(0.05)
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.02)
